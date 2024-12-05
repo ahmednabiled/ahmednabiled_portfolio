423df00f64b1624df24a8d3d0a57163089477fcb
Ahmed Nabil <hhtg250@gmail.com>
2024-12-05 10:48:19 +0200

    fix

--- src/components/notes/CreateNoteDialog.tsx
+++ src/components/notes/CreateNoteDialog.tsx
@@ -65,6 +65,11 @@ export const CreateNoteDialog = ({ open, onOpenChange }: CreateNoteDialogProps) 
                 >
                   <X className="w-5 h-5 text-gray-400" />
                 </button>
+              </div>
+            </div>
+            <br />
+              <center>
+                <div>
                 <button
                   type="submit"
                   className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 
@@ -72,8 +77,8 @@ export const CreateNoteDialog = ({ open, onOpenChange }: CreateNoteDialogProps) 
                 >
                   {translations.addNote[language]}
                 </button>
-              </div>
-            </div>
+                </div>
+              </center>
           </form>
         </Dialog.Content>
       </Dialog.Portal>
